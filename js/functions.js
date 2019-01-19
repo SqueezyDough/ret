4eb033f6be344986a9ebaece52d139ac6a7faf13
SqueezyDough <lvanbiljouw@hotmail.com>
2019-01-19 21:50:12 +0100

    fix

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 const btn_older = document.querySelector('main ul li:nth-of-type(2) button:first-child');
 const btn_newest = document.querySelector('main ul li:nth-of-type(2) button:last-child');
+const btn_group = document.querySelector('main ul li:nth-of-type(2)');
 
 btn_older.addEventListener("click", function(){
   document.getElementById('list-stories').scrollIntoView({
@@ -8,6 +9,7 @@ btn_older.addEventListener("click", function(){
  
   this.classList.add('text-active');
   btn_newest.classList.remove('text-active');
+  btn_group.classList.add('push-down');
 });
 
 btn_newest.addEventListener("click", function(){
@@ -17,5 +19,6 @@ btn_newest.addEventListener("click", function(){
  
   this.classList.add('text-active');
   btn_older.classList.remove('text-active');
+  btn_group.classList.remove('push-down');
 });
 
