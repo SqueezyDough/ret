1dcb14b1047ca87f9d53c52671314f819cd350a2
SqueezyDough <lvanbiljouw@hotmail.com>
2019-01-20 22:29:11 +0100

    added story content -- code

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,142 @@
-const btn_older = document.querySelector('main ul li:first-child >div>div button:first-child');
-const btn_newest = document.querySelector('main ul li:first-child  >div>div button:last-child');
-const btn_group = document.querySelector('main ul li:first-child >div>div');
+const btnOlder = document.querySelector('main ul li:first-child >div>div button:first-child');
+const btnNewest = document.querySelector('main ul li:first-child  >div>div button:last-child');
+const btnGroup = document.querySelector('main ul li:first-child >div>div');
 
-btn_older.addEventListener("click", function(){
+
+// story - CODE
+const storyContainer = document.querySelector('#story-container p');
+
+const fakeHashKey = document.querySelector('#story-container h1>span:nth-child(1)');
+const hashKey_1 = document.querySelector('#story-container h1>span:nth-child(2)');
+const hashKey_2 = document.querySelector('#story-container h1>span:nth-child(3)');
+const hashKey_3 = document.querySelector('#story-container h1>span:nth-child(4)');
+
+var hashKey_1_Owner = false;
+var hashKey_2_Owner = false;
+var hashKey_3_Owner = false;
+var msgDecrypted = false;
+
+var storyContent = "In kwellende situaties toveren apen pennenvruchten uit invloedrijke tapijthandelaren. De engelen buigen uitermate succesvol en negeren langzaam onze overrijpe politieke overlevingstrategieën. Verveelde en radicaal hompiekurkende ettertjes tegelen slablaadjes tot appelmoes. Tevens, in overleg, nagelen schoolverlaters patatjes. Leipe eekhoorntjes in Napels, met immorele jaknikkers naaien ze ogenblikkelijk, ontegenzeggelijk negerzoenen. Laaiend in grote Tefalpannen. Iedere nestor ziet in jaarringen nevels. Klote. Iemand nestelt de eerste relschoppers wat aardbeienijs. Geleidelijk etend negeren Tellsellkijkers een snelkookpan. Laat andere potloodventers een nagelschaartje halen! En tepelkloven redden Elsbeth geenszins. Elke non tegen elke non haalt elf tegelzetters. Want anders aait iemand Theo. De elf meningen etsen niets sympathieks. En nog kopen Ieren juichend katholieke enkelbandjes?! Negen andere aambeelden aan rariteitenkabinetjes belegeren elf negers. Evenzoveel Denen en Noren. Alles aan nietszeggende hoofden ezelt treinverkeer en inboedelverzekeraars. Nog dagelijks veilt Anton nepbontjassen. Help, eendracht tekent plantsoenendiensten! Leer eigenwijsheid in neofoetale mammoettankers of eet taugé. Ik kook over van energie, ruik elke eeltplek, nopende puist, lachende aambei. Nog kunnen velen eerder roken dan etteren. Raar maar eventueel toch aanbevelenswaardig. Als niemand beschonken en individueel de eekhoorntjes kapot ademt. Nooit te eenvoudig neuzen, daapse ingenieurs! Eerst poepen en bouten. Of uitwaaien. Weliswaar plukt u teelballen, tyfuszakjes en neuspuisten. In kraakheldere bewoordingen ebt Nico overige paaseitjes weg. En goed! Niemand anders aardt raar, Duitsers excluded. Buiten invloederijke brekebeentjes lobby ik ordentelijk tot halverwege Enkhuizen. En kijk omstreeks morgenochtend steevast Telekids, in jeugdigheid lekker onovertroffen. Egels fotosynthetiseren eventjes niet in naaldwouden. Geen enkele noot van anderen negeert Queneau. U etaleert nieuwe enkelbandjes. Ach, u treitert elfjes. Leendert en Nico eten noedels. Vretend laten arendsjongen konijnenbotjes vallen op ordentelijk ratten die aanvankelijk toneelstukjes instuderen. Krijg de Engelse bibbers, indigolul. En blijf in nevelen gehuld, anale hopjesliefhebber. Ongelooflijke uilebal. Druilerige theezakjesuitknijper. Elke eenvoudige neringhouder vouwt rouwkorsten om uw wijf, maar ik jaag stuipen tot aanstaande aarswonden! Nertsen dragende eikelvreters. Zelden echt willen indoctrineren. Lepelend welt Eddy teennagels. En niet olieverfachtige, feeërieke diepvrieskippenpootjes in tapirsaus. Helemaal echwel toppie! Panfluiters openen stationsrestauratie, tevens kiosk. Aan niemand toefluisteren. Of ordentelijk raadgeven. In sociale tijden eten rillende wolven ijlende jongen. Later is korter, weet iedereen. Laat u inenten tegen laadkleppen en genetische grogs. En niemand weet alles. Amper reed ze elders naartoe. Amper aardt regenval tot oefenwatertjes en metawatervallen of elke troepoudste katapulteert iemand. Ja, klote toch?! Zelf eten arendsjongen chocoladetruffels. Hij tekent echter retegoed op muren, wanden, aardewerk, appelbomen, roterende eenpersoonspannetjes. En niemand anders urineert tegen Otto! Weet elke gordijnverkoper. Rijdend is Jean diep tevreden. Zoë etaleert rennend een niet te weerstaan elfde gebodje. Bezemend of vervend, en Nico is sympathiek. Het eetlustvervangende tepeltje betaalt otzet enerverende kopermunten. Nog elf reigers grijpen ergens Nico’s strot tot eekhoorntjesbrood. Veel indigo’s neuken daapse echtbrekers. Nog volgens onder liggende gansjes eert niemand Simon, de eftelingkenner. Computers openen mega-store, peuter uilebalt traag en raar in sloten. Help eenvoudige tuinbroekjes bij oefensessies. Een kraaloogjesverzamelaar eert ronduit wel. Ezels, leipo’s. In kooigevechten grijpt Anton naar ambivalente aalscholvers. Reutelend en enerverend neukt Medvedev Engelse diplomaten en weteringcircuitminnende, eensgezinde radicalen. Krijg eerdaags rabiës! Of ook Kleverman het in jijbakken kan aantonen negeert het eventuele tranentrekkende boekwerkje of eenheidsworst kan nog internationaliseren. Enerzijds tyfus (vrienden, indianen, negers, denen, Engelsen, Noren, Hunnen, Indiërs, jodens, zieners, elfjes, godenzonene, trekpaarden, dazen), anderzijds tentharingen. In kogelronde e-mails entert Nico meeldraden aan Italiaanse luipaardvangers. Nog aardige, algehele rariteiten. Het eventuele tegenzetje mag anderzijds graag apentrots zitten immobileren. Ja, nee, maar… Of elcerlyk tot sauerkraut tuurt: u roept en noopt. Dralend iemand emotioneel hoereren. Evenveel buisposten beven eventjes nog na. Of gooit een Engelsman nooit, ever. Xylofonen eroderen metterjaren paradijsachtig. Levend aarden aardappelen religieuzer. Meersterlijke aria’s ademen rare vogels. Ogenschijnlijk lijken gevers en nemers stichtende heilzoekers. Even toch meegeroken aan grote azijn zeikende, in Juinen naaiende ladenlichtende, invloedrijke grootinquisiteur! Tot het eventuele tekentafeltje behoeden ochtenderecties eikels. Kut, God… en waar onzin ontstaat, neigen waarheidszoekers afkeurend achterop rakend het ene terloopse hoorspel of operette reutelend tegen te episteren. Luie indianen geven graag. En nu. Iemand negeert eventjes een babywinkel aan babykleertjes. Yoghurt waait in nevenliggende kelders. Eelt ligt op Perzische hevelvoetjes en trommelt broze omstanders. Slaapverwekkende engeltjes negeren liever ons mensen met eeltbulten. Radeloos punnikt Lorelei eenhoorns in negligé’s. Klootzak, of ondoorgrondelijke penis? Ik koop babyschoentjes aan Bosch Y Schaduw. Chocolade hoentjes om even niets te jonassen. En stiekem vreet Olaf onze radijsjes. Men neme zuigzoenen onder octrooi. Nu!";
+var words = storyContent.split(" ");
+
+// voeg een spatie toe na elk woord
+for (var i = 0; i < words.length - 1; i++) {
+  words[i] += " ";
+}
+
+// zet elk woord in een span en voeg het toe aan de container
+for (var i = 0; i < words.length; i++) {
+  var elRemainder = document.createElement('div');
+  var elFirstChar = document.createElement('div');
+  var elWord = document.createElement('span');
+
+  var currentWord = words[i];
+  var wordFirstChar = currentWord.substring(0,1);
+  var wordRemainder = currentWord.substring(1, currentWord.length); 
+  
+  for (var x = 0; x < wordRemainder.length; x++) {  
+      elRemainderFragment = document.createElement('span');;
+      elRemainderFragment.innerHTML = wordRemainder[x]; 
+      elRemainderFragment.classList.add('encrypted-char');  
+
+      elRemainder.append(elRemainderFragment);  
+  }
+
+  elFirstChar.innerHTML = wordFirstChar;
+
+  elWord.append(elFirstChar);
+  elWord.append(elRemainder);
+
+  storyContainer.append(elWord);
+}
+
+fakeHashKey.addEventListener('click', function(){
+  hashKey_1_Owner = false, hashKey_2_Owner = false, hashKey_3_Owner = false;
+
+  if (!msgDecrypted){
+    hashKey_1.classList.remove('decrypt-char');
+    hashKey_2.classList.remove('decrypt-char');
+    hashKey_3.classList.remove('decrypt-char');
+  }
+
+  console.log('decryption failed')
+  console.log('hKey_1: ' + hashKey_1_Owner, 'hKey_2: ' + hashKey_2_Owner, 'hKey_3: ' + hashKey_3_Owner)
+});
+
+hashKey_1.addEventListener('click', function(){
+  hashKey_1_Owner = true;
+  this.classList.add('decrypt-char');
+  attemptDescryption();
+});
+
+hashKey_2.addEventListener('click', function(){
+  hashKey_2_Owner = true;
+  this.classList.add('decrypt-char');
+  attemptDescryption();
+});
+
+hashKey_3.addEventListener('click', function(){
+  hashKey_3_Owner = true;
+  this.classList.add('decrypt-char');
+  attemptDescryption();
+});
+
+function attemptDescryption(){
+  if(hashKey_1_Owner && hashKey_2_Owner && hashKey_3_Owner){
+    msgDecrypted = true;
+
+    var mgsSuccess = 'racked';  
+    fakeHashKey.innerHTML += mgsSuccess; 
+    
+    const successMsg = document.querySelector('#story-container h2');
+    successMsg.innerHTML = '52.3579946, 4.8686484, 29.01.19, 13:00';
+
+    decrypt_code();
+    console.log('message decrypted');
+  }
+  else{
+    console.log('decryption failed')
+    console.log('hKey_1: ' + hashKey_1_Owner, 'hKey_2: ' + hashKey_2_Owner, 'hKey_3: ' + hashKey_3_Owner)
+  }
+}
+
+function decrypt_code(){
+  var encrypted_chars = document.getElementsByClassName('encrypted-char');
+  var delaysMilliSec = [200, 300, 400, 500];
+
+  for (var i = 0; i < encrypted_chars.length; i++) { 
+    delayOptions = delaysMilliSec.length-1;
+    delay = Math.floor((Math.random() * delayOptions) +1);  
+
+    addDecryption(encrypted_chars[i], delaysMilliSec[delay]);  
+  }
+}
+
+function addDecryption(char, delay){
+  console.log(delay);
+
+  setTimeout(function(){
+    char.classList.add('decrypt-char');
+  }, delay);  
+}
+
+
+btnOlder.addEventListener("click", function(){
   document.getElementById('list-stories').scrollIntoView({
     behavior: 'smooth'
   });
  
   this.classList.add('text-active');
-  btn_newest.classList.remove('text-active');
-  btn_group.classList.add('push-down');
+  btnNewest.classList.remove('text-active');
+  btnGroup.classList.add('push-down');
 });
 
-btn_newest.addEventListener("click", function(){
+btnNewest.addEventListener("click", function(){
   document.getElementById('prev_anchor').scrollIntoView({
     behavior: 'smooth'
   });
  
   this.classList.add('text-active');
-  btn_older.classList.remove('text-active');
-  btn_group.classList.remove('push-down');
+  btnOlder.classList.remove('text-active');
+  btnGroup.classList.remove('push-down');
 });
-
