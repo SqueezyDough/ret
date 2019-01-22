6f41656ec3500ada49af69b43e543e5465881bad
SqueezyDough <lvanbiljouw@hotmail.com>
2019-01-22 02:28:01 +0100

    added comments, comment form, credits

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,6 @@ const btnOlder = document.querySelector('main ul li:first-child >div>div button:
 const btnNewest = document.querySelector('main ul li:first-child  >div>div button:last-child');
 const btnGroup = document.querySelector('main ul li:first-child >div>div');
 
-
 // story - CODE
 const storyContainer = document.querySelector('#story-container p');
 
@@ -16,7 +15,9 @@ var hashKey_2_Owner = false;
 var hashKey_3_Owner = false;
 var msgDecrypted = false;
 
+// content van het verhaal
 var storyContent = "In kwellende situaties toveren apen pennenvruchten uit invloedrijke tapijthandelaren. De engelen buigen uitermate succesvol en negeren langzaam onze overrijpe politieke overlevingstrategieën. Verveelde en radicaal hompiekurkende ettertjes tegelen slablaadjes tot appelmoes. Tevens, in overleg, nagelen schoolverlaters patatjes. Leipe eekhoorntjes in Napels, met immorele jaknikkers naaien ze ogenblikkelijk, ontegenzeggelijk negerzoenen. Laaiend in grote Tefalpannen. Iedere nestor ziet in jaarringen nevels. Klote. Iemand nestelt de eerste relschoppers wat aardbeienijs. Geleidelijk etend negeren Tellsellkijkers een snelkookpan. Laat andere potloodventers een nagelschaartje halen! En tepelkloven redden Elsbeth geenszins. Elke non tegen elke non haalt elf tegelzetters. Want anders aait iemand Theo. De elf meningen etsen niets sympathieks. En nog kopen Ieren juichend katholieke enkelbandjes?! Negen andere aambeelden aan rariteitenkabinetjes belegeren elf negers. Evenzoveel Denen en Noren. Alles aan nietszeggende hoofden ezelt treinverkeer en inboedelverzekeraars. Nog dagelijks veilt Anton nepbontjassen. Help, eendracht tekent plantsoenendiensten! Leer eigenwijsheid in neofoetale mammoettankers of eet taugé. Ik kook over van energie, ruik elke eeltplek, nopende puist, lachende aambei. Nog kunnen velen eerder roken dan etteren. Raar maar eventueel toch aanbevelenswaardig. Als niemand beschonken en individueel de eekhoorntjes kapot ademt. Nooit te eenvoudig neuzen, daapse ingenieurs! Eerst poepen en bouten. Of uitwaaien. Weliswaar plukt u teelballen, tyfuszakjes en neuspuisten. In kraakheldere bewoordingen ebt Nico overige paaseitjes weg. En goed! Niemand anders aardt raar, Duitsers excluded. Buiten invloederijke brekebeentjes lobby ik ordentelijk tot halverwege Enkhuizen. En kijk omstreeks morgenochtend steevast Telekids, in jeugdigheid lekker onovertroffen. Egels fotosynthetiseren eventjes niet in naaldwouden. Geen enkele noot van anderen negeert Queneau. U etaleert nieuwe enkelbandjes. Ach, u treitert elfjes. Leendert en Nico eten noedels. Vretend laten arendsjongen konijnenbotjes vallen op ordentelijk ratten die aanvankelijk toneelstukjes instuderen. Krijg de Engelse bibbers, indigolul. En blijf in nevelen gehuld, anale hopjesliefhebber. Ongelooflijke uilebal. Druilerige theezakjesuitknijper. Elke eenvoudige neringhouder vouwt rouwkorsten om uw wijf, maar ik jaag stuipen tot aanstaande aarswonden! Nertsen dragende eikelvreters. Zelden echt willen indoctrineren. Lepelend welt Eddy teennagels. En niet olieverfachtige, feeërieke diepvrieskippenpootjes in tapirsaus. Helemaal echwel toppie! Panfluiters openen stationsrestauratie, tevens kiosk. Aan niemand toefluisteren. Of ordentelijk raadgeven. In sociale tijden eten rillende wolven ijlende jongen. Later is korter, weet iedereen. Laat u inenten tegen laadkleppen en genetische grogs. En niemand weet alles. Amper reed ze elders naartoe. Amper aardt regenval tot oefenwatertjes en metawatervallen of elke troepoudste katapulteert iemand. Ja, klote toch?! Zelf eten arendsjongen chocoladetruffels. Hij tekent echter retegoed op muren, wanden, aardewerk, appelbomen, roterende eenpersoonspannetjes. En niemand anders urineert tegen Otto! Weet elke gordijnverkoper. Rijdend is Jean diep tevreden. Zoë etaleert rennend een niet te weerstaan elfde gebodje. Bezemend of vervend, en Nico is sympathiek. Het eetlustvervangende tepeltje betaalt otzet enerverende kopermunten. Nog elf reigers grijpen ergens Nico’s strot tot eekhoorntjesbrood. Veel indigo’s neuken daapse echtbrekers. Nog volgens onder liggende gansjes eert niemand Simon, de eftelingkenner. Computers openen mega-store, peuter uilebalt traag en raar in sloten. Help eenvoudige tuinbroekjes bij oefensessies. Een kraaloogjesverzamelaar eert ronduit wel. Ezels, leipo’s. In kooigevechten grijpt Anton naar ambivalente aalscholvers. Reutelend en enerverend neukt Medvedev Engelse diplomaten en weteringcircuitminnende, eensgezinde radicalen. Krijg eerdaags rabiës! Of ook Kleverman het in jijbakken kan aantonen negeert het eventuele tranentrekkende boekwerkje of eenheidsworst kan nog internationaliseren. Enerzijds tyfus (vrienden, indianen, negers, denen, Engelsen, Noren, Hunnen, Indiërs, jodens, zieners, elfjes, godenzonene, trekpaarden, dazen), anderzijds tentharingen. In kogelronde e-mails entert Nico meeldraden aan Italiaanse luipaardvangers. Nog aardige, algehele rariteiten. Het eventuele tegenzetje mag anderzijds graag apentrots zitten immobileren. Ja, nee, maar… Of elcerlyk tot sauerkraut tuurt: u roept en noopt. Dralend iemand emotioneel hoereren. Evenveel buisposten beven eventjes nog na. Of gooit een Engelsman nooit, ever. Xylofonen eroderen metterjaren paradijsachtig. Levend aarden aardappelen religieuzer. Meersterlijke aria’s ademen rare vogels. Ogenschijnlijk lijken gevers en nemers stichtende heilzoekers. Even toch meegeroken aan grote azijn zeikende, in Juinen naaiende ladenlichtende, invloedrijke grootinquisiteur! Tot het eventuele tekentafeltje behoeden ochtenderecties eikels. Kut, God… en waar onzin ontstaat, neigen waarheidszoekers afkeurend achterop rakend het ene terloopse hoorspel of operette reutelend tegen te episteren. Luie indianen geven graag. En nu. Iemand negeert eventjes een babywinkel aan babykleertjes. Yoghurt waait in nevenliggende kelders. Eelt ligt op Perzische hevelvoetjes en trommelt broze omstanders. Slaapverwekkende engeltjes negeren liever ons mensen met eeltbulten. Radeloos punnikt Lorelei eenhoorns in negligé’s. Klootzak, of ondoorgrondelijke penis? Ik koop babyschoentjes aan Bosch Y Schaduw. Chocolade hoentjes om even niets te jonassen. En stiekem vreet Olaf onze radijsjes. Men neme zuigzoenen onder octrooi. Nu!";
+// haal hier alle woorden uit en stip het in een lijst
 var words = storyContent.split(" ");
 
 // voeg een spatie toe na elk woord
@@ -26,30 +27,38 @@ for (var i = 0; i < words.length - 1; i++) {
 
 // zet elk woord in een span en voeg het toe aan de container
 for (var i = 0; i < words.length; i++) {
+  // maak de elementen aan waarin we de content stoppen
   var elRemainder = document.createElement('div');
   var elFirstChar = document.createElement('div');
   var elWord = document.createElement('span');
 
   var currentWord = words[i];
+  // scheid eerste char met de rest van het woord
   var wordFirstChar = currentWord.substring(0,1);
   var wordRemainder = currentWord.substring(1, currentWord.length); 
   
+  // maak voor elke charachter na de eerste een aparte span en voeg het toe
   for (var x = 0; x < wordRemainder.length; x++) {  
       elRemainderFragment = document.createElement('span');;
       elRemainderFragment.innerHTML = wordRemainder[x]; 
       elRemainderFragment.classList.add('encrypted-char');  
 
+      // stop de spannetjes in de remainder
       elRemainder.append(elRemainderFragment);  
   }
 
+  // stop het woord in de container
   elFirstChar.innerHTML = wordFirstChar;
 
+  // voeg de 2 delen bij elkaar
   elWord.append(elFirstChar);
   elWord.append(elRemainder);
 
+  // voeg het hele woord toe aan de container
   storyContainer.append(elWord);
 }
 
+// als je klikt op de fakehashkey, is de decryption gefaald
 fakeHashKey.addEventListener('click', function(){
   hashKey_1_Owner = false, hashKey_2_Owner = false, hashKey_3_Owner = false;
 
@@ -63,37 +72,46 @@ fakeHashKey.addEventListener('click', function(){
   console.log('hKey_1: ' + hashKey_1_Owner, 'hKey_2: ' + hashKey_2_Owner, 'hKey_3: ' + hashKey_3_Owner)
 });
 
+// bemachtig de hashkey en probeer om te decrypten
 hashKey_1.addEventListener('click', function(){
   hashKey_1_Owner = true;
   this.classList.add('decrypt-char');
   attemptDescryption();
 });
 
+// bemachtig de hashkey en probeer om te decrypten
 hashKey_2.addEventListener('click', function(){
   hashKey_2_Owner = true;
   this.classList.add('decrypt-char');
   attemptDescryption();
 });
 
+// bemachtig de hashkey en probeer om te decrypten
 hashKey_3.addEventListener('click', function(){
   hashKey_3_Owner = true;
   this.classList.add('decrypt-char');
   attemptDescryption();
 });
 
+// probeer te decrypten
 function attemptDescryption(){
+  // als je alle hashkeys bemachtigd hebt, kun je het bericht decrypten
   if(hashKey_1_Owner && hashKey_2_Owner && hashKey_3_Owner){
     msgDecrypted = true;
 
+    // success feedback
     var mgsSuccess = 'racked';  
     fakeHashKey.innerHTML += mgsSuccess; 
     
+    // voeg coordinaten toe voor je afspraak met de verwarde man
     const successMsg = document.querySelector('#story-container h2');
     successMsg.innerHTML = '52.3579946, 4.8686484, 29.01.19, 13:00';
 
+    // trigger decruption
     decrypt_code();
     console.log('message decrypted');
   }
+  // decryption failed, log key owners
   else{
     console.log('decryption failed')
     console.log('hKey_1: ' + hashKey_1_Owner, 'hKey_2: ' + hashKey_2_Owner, 'hKey_3: ' + hashKey_3_Owner)
@@ -102,19 +120,19 @@ function attemptDescryption(){
 
 function decrypt_code(){
   var encrypted_chars = document.getElementsByClassName('encrypted-char');
+  // elke character wordt met verschillende delays gedecrypt
   var delaysMilliSec = [200, 300, 400, 500];
 
   for (var i = 0; i < encrypted_chars.length; i++) { 
     delayOptions = delaysMilliSec.length-1;
     delay = Math.floor((Math.random() * delayOptions) +1);  
 
+    // voeg css class toe voor decryption met de delay
     addDecryption(encrypted_chars[i], delaysMilliSec[delay]);  
   }
 }
 
 function addDecryption(char, delay){
-  console.log(delay);
-
   setTimeout(function(){
     char.classList.add('decrypt-char');
   }, delay);  
